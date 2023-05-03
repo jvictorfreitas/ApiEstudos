86102755f2467975dfd24f089b132e289e0f3f2f
jvictorfreitas <jvictorfreiasn@gmail.com>
2023-05-03 18:45:19 -0300

    Implementação da camada DTO

---
+++ Application/DTOs/Validations/PersonDTOValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Validations
+{
+    internal class PersonDTOValidator : AbstractValidator<PersonDTO>
+    {
+        public PersonDTOValidator() 
+        {
+            RuleFor(x => x.Document).NotEmpty().NotNull().WithMessage("Documento deve ser informado!");
+            RuleFor(x => x.Name).NotEmpty().NotNull().WithMessage("Nome deve ser informado!");
+            RuleFor(x => x.Phone).NotEmpty().NotNull().WithMessage("Telefone deve ser informado!");
+        }
+    }
+}
